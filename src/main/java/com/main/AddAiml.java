2f0ec67e6b2319b6cb0278d925c8b4b8e4f0340b
etkred <52811015+etkred@users.noreply.github.com>
2019-09-09 01:05:19 +0530

    Update AddAiml.java

--- src/main/java/com/main/AddAiml.java
+++ src/main/java/com/main/AddAiml.java
@@ -3,26 +3,24 @@
  */
 package com.main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.alicebot.ab.Bot;
 import com.main.util.ResourcePath;
 
 public class AddAiml {
 
   static final String BOTNAME = "super";
+  private static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
   public static void main(String[] args) {
-    try {
-
-      final String resourcesPath = ResourcePath.getResourcesPath();
-      final Bot bot = new Bot(BOTNAME, resourcesPath);
-
-      bot.writeAIMLFiles();
-
-    } catch (final Exception e) {
-      e.printStackTrace();
-    }
+    updateAiml();
   }
-
-
+  public static boolean updateAiml() {
+    final Bot bot = new Bot(BOTNAME, ResourcePath.getResourcesPath());
+    logger.log(Level.INFO, "adding aiml successful");
+    bot.writeAIMLFiles();
+    return true;
+  }
 }
 
