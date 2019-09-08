b2c29ed9969b1bcc6aa250ccfb7d51bcef9c6461
etkred <52811015+etkred@users.noreply.github.com>
2019-09-09 00:57:25 +0530

    Update AutoBuildTest.bat

--- AutoBuildTest.bat
+++ AutoBuildTest.bat
@@ -7,8 +7,4 @@ java -jar .\lib\simian.jar -threshold=3 .\src\**\*.java -excludes=**\*Test.java
 echo -------------------------------------------
 call mvn clean
 call mvn install
-start /min "" .\target\jacoco-ut\index.html
-echo press Ctrl + c to stop the server
-
-call mvn spring-boot:run
 pause
