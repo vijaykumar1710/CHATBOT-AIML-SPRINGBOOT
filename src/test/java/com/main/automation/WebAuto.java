9b50937a84f9ec05a0cc99ce49c37ff03db33ff5
etkred <52811015+etkred@users.noreply.github.com>
2019-09-09 11:15:53 +0530

    Update WebAuto.java

--- src/test/java/com/main/automation/WebAuto.java
+++ src/test/java/com/main/automation/WebAuto.java
@@ -1,8 +1,7 @@
 /*
  * The copyright of this file belongs to Koninklijke Philips N.V., 2019.
- */
-
-package com.main.automation;
+// */
+package com.main.uitest;
 
 import java.util.Random;
 import org.openqa.selenium.By;
@@ -11,39 +10,46 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebAuto {
-  public static void main(String[] args) {
-    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+
+  public static String automate(String hospitalType, String acuity, String screenType, String screenSize, String addons){
+    System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+    final String URL = "http://localhost:8085/user.jsp";
     final WebDriver driver = new ChromeDriver();
-    driver.get("http://localhost:8085/user.jsp");
+    driver.get(URL);
     final Random rand = new Random();
-    final String randusername = "test" + rand.nextInt(100);
+    final String randusername = "test" + rand.nextInt(1000) ;
     final WebElement username = driver.findElement(By.name("username"));
     final WebElement contact = driver.findElement(By.name("contact"));
     final WebElement submit = driver.findElement(By.id("submit"));
     username.sendKeys(randusername);
-    contact.sendKeys("923456790");
+    contact.sendKeys("9234567890");
     submit.click();
 
+    final String inputId = "my-input";
+    final String submitId = "submit-input";
+    driver.findElement(By.id(inputId)).sendKeys("I am looking for PMS devices");
+    driver.findElement(By.id(submitId)).click();
+    driver.findElement(By.id(inputId));
+    driver.findElement(By.id(submitId)).click();
+    driver.findElement(By.id(inputId)).sendKeys(hospitalType);
+    driver.findElement(By.id(submitId)).click();
+    driver.findElement(By.id(inputId)).sendKeys(acuity);
+    driver.findElement(By.id(submitId)).click();
+    driver.findElement(By.id(inputId)).sendKeys(screenType);
+    driver.findElement(By.id(submitId)).click();
+    driver.findElement(By.id(inputId)).sendKeys(screenSize);
+    driver.findElement(By.id(submitId)).click();
+    driver.findElement(By.id(inputId)).sendKeys("addons: " + addons);
+    driver.findElement(By.id(submitId)).click();
+    driver.findElement(By.id(inputId)).sendKeys("ok show the product");
+    driver.findElement(By.id(submitId)).click();
+    final String Suggested = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[1]")).getText();
+    driver.findElement(By.id("msg")).sendKeys("interested");
+    driver.findElement(By.id("getresult")).click();
+    driver.switchTo().alert().accept();
+    driver.quit();
+    return Suggested;
 
-    driver.findElement(By.id("my-input")).sendKeys("I am looking for PMS devices");
-    driver.findElement(By.id("submit-input")).click();
-    driver.findElement(By.id("my-input"));
-    driver.findElement(By.id("submit-input")).click();
-    driver.findElement(By.id("my-input")).sendKeys("tier 1");
-    driver.findElement(By.id("submit-input")).click();
-    driver.findElement(By.id("my-input")).sendKeys("Mid Acuity");
-    driver.findElement(By.id("submit-input")).click();
-    driver.findElement(By.id("my-input")).sendKeys("touch screen");
-    driver.findElement(By.id("submit-input")).click();
-    driver.findElement(By.id("my-input")).sendKeys("12 inch");
-    driver.findElement(By.id("submit-input")).click();
-    driver.findElement(By.id("my-input")).sendKeys("addons: SpO2");
-    driver.findElement(By.id("submit-input")).click();
-    driver.findElement(By.id("my-input")).sendKeys("ok show the product");
-    driver.findElement(By.id("submit-input")).click();
-    final WebElement col = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[1]"));
-    System.out.println(col.getText());
   }
-
 }
-
